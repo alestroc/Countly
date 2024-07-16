4214e4a50bbb1bad40e1787b8bbfbb062b765ed3
alestroc <alestroc@gmail.com>
2024-07-16 15:54:21 +0200

    (add): cardFavoriteEvent

--- App.tsx
+++ App.tsx
@@ -2,21 +2,18 @@ import React from 'react';
 import {SafeAreaView, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import CardFavoriteEvent from './src/components/cardFavoriteEvent';
 
 import HomeScreen from './src/screens/home';
 import EditScreen from './src/screens/edit';
+import GeneralButton from './src/components/generalButton';
 
 const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
   return (
     <>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Edit" component={EditScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <CardFavoriteEvent />
     </>
   );
 }
---
+++ src/components/cardFavoriteEvent.tsx
@@ -0,0 +1,62 @@
+import {StyleSheet, Text, View, Image} from 'react-native';
+import React from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+const CardFavoriteEvent = ({
+  imgUrl,
+  title,
+  location,
+  date,
+  hours,
+  host,
+}: {
+  imgUrl: string;
+  title: string;
+  location: string;
+  date: string;
+  hours: string;
+  host: string;
+}) => {
+  return (
+    <SafeAreaView>
+      <View style={styles.row}>
+        <View>
+          <Image source={{uri: imgUrl}} style={styles.imgArea} />
+        </View>
+        <View style={styles.txtArea}>
+          <Text>Electro Music Festival</Text>
+          <Text>S. Giovanni del Dosso(MN)</Text>
+          <Text>sab 25 Dic - pre 21:30</Text>
+
+          <Text>"Soundsystem Verona"</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default CardFavoriteEvent;
+
+const styles = StyleSheet.create({
+  row: {
+    borderWidth: 2,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: 342,
+    height: 161,
+  },
+  imgArea: {
+    borderWidth: 2,
+    width: 109,
+    height: 161,
+    flex: 1,
+  },
+  txtArea: {
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    alignSelf: 'center',
+    borderWidth: 2,
+    width: 200,
+    height: 125,
+  },
+});
