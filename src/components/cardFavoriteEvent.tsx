7a51880163fe1cd5ae5e4dc3da77d7a2cb6d8a71
alestroc <alestroc@gmail.com>
2024-07-16 15:58:15 +0200

    fix

--- src/components/cardFavoriteEvent.tsx
+++ src/components/cardFavoriteEvent.tsx
@@ -21,7 +21,12 @@ const CardFavoriteEvent = ({
     <SafeAreaView>
       <View style={styles.row}>
         <View>
-          <Image source={{uri: imgUrl}} style={styles.imgArea} />
+          <Image
+            source={{
+              uri: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fhtesting-i03.mycdn.no%2Fmysimgprod%2Fhtesting_mystore_no%2Fimages%2F51948_Fastkarm_Vindu_110x160__109x159__1.png%2Fw600h600.png&f=1&nofb=1&ipt=08c5a8bd924839b55bb07e888be5f9540f6e690dcd9f9c4967f150d5f8cb5682&ipo=images',
+            }}
+            style={styles.imgArea}
+          />
         </View>
         <View style={styles.txtArea}>
           <Text>Electro Music Festival</Text>
