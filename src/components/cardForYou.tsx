b50098b1808942cfb923718eaeae743a56f68a61
alestroc <alestroc@gmail.com>
2024-07-25 16:13:47 +0200

    animazione base cardForYou

--- src/components/cardForYou.tsx
+++ src/components/cardForYou.tsx
@@ -1,12 +1,32 @@
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
-import Animatedfrom, {useSharedValue} from 'react-native-reanimated';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+} from 'react-native-reanimated';
 
 const CardForYou = ({nEventi}: {nEventi: string}) => {
+  const height = useSharedValue(150);
+
+  const handlePress = () => {
+    height.value = withSpring(height.value + 50);
+    console.log('Premuto');
+  };
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      height: height.value,
+    };
+  });
+
   return (
     <View style={styles.main}>
-      <TouchableOpacity style={styles.config1} />
-      <Text style={styles.config2}>{nEventi} Eventi</Text>
+      <Animated.View style={[styles.animatedView, animatedStyle]}>
+        <TouchableOpacity onPress={handlePress} style={styles.config1}>
+          <Text style={styles.config2}>{nEventi} Eventi</Text>
+        </TouchableOpacity>
+      </Animated.View>
     </View>
   );
 };
@@ -19,25 +39,21 @@ const styles = StyleSheet.create({
     width: '100%',
     bottom: 0,
   },
+  animatedView: {
+    width: '100%',
+    backgroundColor: 'lightgrey',
+  },
   config1: {
     borderWidth: 2,
-
     position: 'relative',
     top: 8,
     backgroundColor: 'lightgrey',
-    height: 20,
-    width: '25%',
+    height: 200,
+    width: '100%',
     alignSelf: 'center',
-    borderRadius: 16,
   },
   config2: {
-    borderWidth: 2,
-
-    borderTopRightRadius: 16,
-    borderTopLeftRadius: 16,
-    backgroundColor: 'white',
-    height: 60,
     textAlign: 'center',
-    textAlignVertical: 'center',
+    marginTop: 10,
   },
 });
